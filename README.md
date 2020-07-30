3d863a40d0995461d9d50a3421fcfcb87c2e65a9
Nikhil Rao <raonikhil57@gmail.com>
2020-07-30 15:18:57 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Table of Contents
+1. [Methodology](#Method)
+1. [DAG](#DAG)
+2. [Questions](#Questions)
+
+## Methodology <a name="Method"></a>
+	* Since the goal of this challenge was to join two streams/sets of data and perform simple aggregate statistics
+	BeamSQL was used for all operations and joins. The CSV data was first cast with a Schema to Row Beam Datatypes, then SQL queries
+	were used to perform operations.
+
+## Directed Acyclic Graph <a name="DAG"></a>
+
+## Questions <a name="Questions"></a>
+
+### If this were an actual project, what steps would you take to get it ready for production?
+    
+    * Javadoc documentation
+    * Continuous Deployment
+    * Continuous Integration
+    * Managed Git Repo -> GCP Cloud Repositories
+    * Airflow Scheduler
+    * Security - API Keys, Oauth, minimum permission scope
+
+### What would you change or improve if you had more time?
+
+    * Design Doc
+    * More Tests
+    * Logging
+    * Performance Profiling
+    
+### If this were an actual project were, the requirements made clear? What could be added or clarified?
+
+	*
+
+### In your experience, what kind of requirements work best? What kind of template would you use to define requirements for a group of developers which you were leading?
+* JIRA Tickets
+* Agile Methodology 
+* The more specific the requirements the better
---
+++ src/main/java/com/nikhilrrao/TapCartPipeline.java
@@ -0,0 +1,143 @@
+package com.nikhilrrao;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.extensions.sql.SqlTransform;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.schemas.transforms.Join;
+import org.apache.beam.sdk.schemas.transforms.Select;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.Sum;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TapCartPipeline {
+	private static final Logger LOG = LoggerFactory.getLogger(TapCartPipeline.class);
+
+	/*
+	 * Define Schema for Event and Device PCollection
+	 */
+
+	static Schema eventSchema = Schema.builder().addStringField("deviceId").addStringField("session")
+			.addStringField("timestamp").addStringField("timstamp_iso")
+			.addNullableField("totalPrice", Schema.FieldType.DECIMAL).addStringField("type").build();
+
+	static Schema deviceSchema = Schema.builder().addStringField("ID").addStringField("User").build();
+
+	static class CastToRow extends DoFn<String, Row> {
+		@ProcessElement
+		public void processElement(@Element String row, OutputReceiver<Row> out) {
+			try {
+				List<String> data = new ArrayList<String>(Arrays.asList(row.split(",")));
+				Row elementRow;
+				if (data.size() == 2) {
+					elementRow = Row.withSchema(deviceSchema).addValues(data.get(0), data.get(1)).build();
+				} else {
+					elementRow = Row.withSchema(eventSchema)
+							.addValues(data.get(0), data.get(1), data.get(2), data.get(3), data.get(4), data.get(5))
+							.build();
+				}
+
+				// Output the Row representing the current POJO
+				out.output(elementRow);
+			} catch (Exception e) {
+				LOG.error("there was an error casting this string to a row: " + row + "\n The error message is: " + e);
+			}
+		}
+	}
+
+	/** A SimpleFunction that converts Count into a printable string. */
+	public static class FormatAsTextFnCount extends SimpleFunction<KV<String, Long>, String> {
+		@Override
+		public String apply(KV<String, Long> input) {
+			return "Total Count of " + input.getKey() + ": " + input.getValue();
+		}
+	}
+
+	/** A SimpleFunction that converts a sum into a printable string. */
+	public static class FormatAsTextFnSum extends SimpleFunction<Double, String> {
+		@Override
+		public String apply(Double input) {
+			return "Total Sum of ADD_TO_CART: " + input;
+		}
+	}
+
+	/** A SimpleFunction that converts a Row into a printable string. */
+	public static class FormatAsTextFnRow extends SimpleFunction<Row, String> {
+		@Override
+		public String apply(Row input) {
+			return input.getValues().toString();
+		}
+	}
+
+	public static void main(String[] args) {
+		Pipeline p = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
+
+		// Process Device Data
+		PCollection<Row> devices = p
+				.apply(TextIO.read().from("gs://49b661f4-6504-4613-8917-c105e720a266/TapcartChallengeData/devices.csv"))
+				.apply(Filter.by((String line) -> !line.isEmpty()))
+				.apply(Filter.by((String line) -> !line.equals("ID,User"))).apply(ParDo.of(new CastToRow()))
+				.setRowSchema(deviceSchema);
+
+		// Process Events Data
+		PCollection<Row> events = p
+				.apply(TextIO.read().from("gs://49b661f4-6504-4613-8917-c105e720a266/TapcartChallengeData/events.csv"))
+				.apply(Filter.by((String line) -> !line.isEmpty()))
+				.apply(Filter
+						.by((String line) -> !line.equals("deviceId,session,timestamp,timstamp_iso,totalPrice,type")))
+				.apply(MapElements.into(TypeDescriptors.strings()).via((String line) -> line.replace("$", "")))
+				.apply(ParDo.of(new CastToRow())).setRowSchema(eventSchema);
+
+		PCollectionTuple eventsAndDevices = PCollectionTuple.of(new TupleTag<>("events"), events)
+				.and(new TupleTag<>("devices"), devices);
+
+		// Create a PCollection for Enriched Events
+		PCollection<Row> EnrichedEvents = eventsAndDevices.apply(SqlTransform
+				.query("SELECT * FROM events INNER JOIN devices ON events.deviceId = devices.ID"));
+
+		EnrichedEvents.apply(MapElements.via(new FormatAsTextFnRow()))
+				.apply(TextIO.write().to("gs://49b661f4-6504-4613-8917-c105e720a266/EnrichedEvents").withoutSharding());
+
+		/*
+		 * // Count all APP_OPENED and ADD_TO_CART events.apply(ParDo.of(new DoFn<Row,
+		 * String>() {
+		 * 
+		 * @ProcessElement public void processElement(@Element Row event,
+		 * OutputReceiver<String> out) { out.output(event.getString("type")); }
+		 * })).apply(Count.<String>perElement()).apply(MapElements.via(new
+		 * FormatAsTextFnCount())).apply( TextIO.write().to(
+		 * "gs://49b661f4-6504-4613-8917-c105e720a266/submission_output").
+		 * withoutSharding());
+		 * 
+		 * // Sum all ADD_TO_CART events.apply(ParDo.of(new DoFn<Row, Double>() {
+		 * 
+		 * @ProcessElement public void processElement(@Element Row event,
+		 * OutputReceiver<Double> out) {
+		 * out.output(event.getDecimal("totalPrice").doubleValue()); }
+		 * })).apply(Sum.doublesGlobally()).apply(MapElements.via(new
+		 * FormatAsTextFnSum())).apply( TextIO.write().to(
+		 * "gs://49b661f4-6504-4613-8917-c105e720a266/submission_output").
+		 * withoutSharding());
+		 */
+
+		p.run();
+	}
+
+}
